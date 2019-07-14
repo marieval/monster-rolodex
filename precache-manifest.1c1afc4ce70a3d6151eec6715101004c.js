0642d73de395e0adfa91c5f9d3cdf2bea7fc4cfb
marieval <majiku@seznam.cz>
2019-07-14 15:25:33 +0200

    Updates

---
+++ precache-manifest.1c1afc4ce70a3d6151eec6715101004c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c78a4979fe420d707a0fc70b5bf8014",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "64522daec1d9c5b186a3",
+    "url": "/monsters-rolodex/static/css/main.e455ba08.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "64522daec1d9c5b186a3",
+    "url": "/monsters-rolodex/static/js/main.283749ba.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
